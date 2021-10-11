c21dda24a33a915bcbbf24d73dde1d2ab73b5b46
Ashish Prasad <56514722+1-Mirage@users.noreply.github.com>
2021-10-11 23:20:24 +0530

    Update cal-istria.js

--- cal-istria.js
+++ cal-istria.js
@@ -4,15 +4,24 @@ const calculate = document.getElementById("calculate");
 const output = document.getElementById("output-answer");
 const third = document.getElementById("thirdside");
 
-
+function reloadpage()
+{
+  location.reload();
+}
 function calcarea(e) 
 {
   e.preventDefault();
   const firstvalue = Number(first.value);
   const secondvalue = Number(second.value);
   const thirdvalue = Number(third.value);
+  if(firstvalue<0||secondvalue<0||thirdvalue<0)
+  {
+    reloadpage();
+    alert("Enter positive value");
+    
+  }
 
-  if ((firstvalue+secondvalue+thirdvalue)===180) 
+  else if ((firstvalue+secondvalue+thirdvalue)===180) 
   {
     output.innerText = `Its a traingle`;
   } else
