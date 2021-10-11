b18272da2a60256708bf9af6381bf5ac5dc93cf9
Ashish Prasad <56514722+1-Mirage@users.noreply.github.com>
2021-10-11 23:19:51 +0530

    Update cal-hypo.js

--- cal-hypo.js
+++ cal-hypo.js
@@ -3,15 +3,26 @@ const second = document.getElementById("secondside");
 const calculate = document.getElementById("calculate");
 const output = document.getElementById("output-answer");
 
-
+function reloadpage()
+{
+  location.reload();
+}
 function calcarea(e) 
 {
   e.preventDefault();
   const firstvalue = Number(first.value);
   const secondvalue = Number(second.value);
-
+  if(firstvalue<0||secondvalue<0)
+  {
+    reloadpage();
+    alert("Enter positive value");
+    
+  }
+  else
+  {
    const thirdvalue=Math.sqrt(firstvalue*firstvalue+secondvalue*secondvalue).toFixed(3);
     output.innerText = `The Hypotenuse is ${thirdvalue}`;
+  }
   
 }
 
